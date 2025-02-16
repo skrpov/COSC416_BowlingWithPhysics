70405959099b2894175f8de016199fa546047844
skrpov <skarpov03@gmail.com>
2025-02-16 02:31:07 -0800

    Add: Count and display fallen pins

---
+++ Assets/Scripts/FallTrigger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FallTrigger : MonoBehaviour
+{
+    public UnityEvent OnPinFall = new();
+    public bool isPinFallen = false;
+    private void OnTriggerEnter(Collider triggeredObject)
+    {
+        if (triggeredObject.CompareTag("Ground") && !isPinFallen)
+        {
+            isPinFallen = true;
+            OnPinFall?.Invoke();
+            Debug.Log($"{gameObject.name} is fallen");
+        }
+    }
+}
--- Assets/Scripts/Gutter.cs
+++ Assets/Scripts/Gutter.cs
@@ -4,6 +4,8 @@ public class Gutter : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.CompareTag("Ground")) 
+            return;
         Rigidbody ballRigidBody = other.GetComponent<Rigidbody>();
         float speed = ballRigidBody.linearVelocity.magnitude;
         ballRigidBody.linearVelocity = Vector3.zero;
