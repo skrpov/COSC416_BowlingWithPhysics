aa59fa4a9103848ff6eeae5e94890f15b9af9892
skrpov <skarpov03@gmail.com>
2025-02-15 23:33:10 -0800

    Add: Player concept

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
     public UnityEvent OnSpacePressed = new UnityEvent();
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+
 
     void Start()
     {
@@ -16,5 +18,16 @@ public class InputManager : MonoBehaviour
         {
             OnSpacePressed?.Invoke();
         }
+
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
     }
 }
